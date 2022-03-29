ccd443d69576ffeb1f464efedbda7e4fa35073d2
Alex-Elias <alex.pohjanpelto@gmail.com>
2022-03-29 23:32:54 +0300

    Add error messages to login and register

--- routes.py
+++ routes.py
@@ -19,7 +19,7 @@ def login():
         password = request.form["password"]
 
         if not users.login(username, password):
-            return render_template("error.html", message="Incorrect username or password")
+            return render_template("login.html", passwordErrorMessage="Incorrect username or password", reusername=username)
         return redirect("/")
 
 
@@ -31,20 +31,22 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         if len(username) < 1 or len(username) > 20:
-            return render_template("error.html", message="The username must be no longer than 20 characters and at least one") #TO DO
+            return render_template("register.html", usernameErrorMessage="The username must be no longer than 20 characters and at least one") #TO DO
 
         password1 = request.form["password"]
         password2 = request.form["repassword"]
 
         if password1 != password2:
-            return render_template("error.html", message="The passwords do not match") # TO DO
+            return render_template("register.html", passwordsDoNotMatchErrorMessage="The passwords do not match", reusername=username) # TO DO
 
         if password1 == "":
-            return render_template("error.html", message="The password must be at least one character long") # TO DO
+            return render_template("register.html", passwordLengthErrorMessage="The password must be at least one character long", reusername=username) # TO DO
 
         if not users.register(username, password1):
-            return render_template("error.html", message="Registration did not work, the username may not be available") # TO DO
+            return render_template("register.html", registrationErrorMessage="Registration did not work, the username may not be available") # TO DO
         return redirect("/login")
+
+
 @app.route("/logout")
 def logout():
     users.logout()
